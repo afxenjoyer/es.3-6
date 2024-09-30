e675b3aa60125724bade35ac01a2fdd2e73c59da
Andrea Di Mingo <dimingo.andrea25@gmail.com>
2024-09-30 10:37:57 +0200

    Corretto il programma

--- main.c
+++ main.c
@@ -16,24 +16,21 @@ int main(void)
     if (prezzoUnitario < 10)
     {
         prezzoScontato = prezzoUnitario;
-        printf("L'importo totale è %.02f\n", prezzoScontato);
-        printf("L'importo non scontato è %.02f\n", prezzoUnitario);
         printf("Lo sconto applicato è del 0%%");
     }
     else if(prezzoUnitario >= 10 && prezzoUnitario <= 49)
     {
         prezzoScontato = prezzoUnitario - (prezzoUnitario/100)*10;
-        printf("L'importo totale è %.02f\n", prezzoScontato);
-        printf("L'importo non scontato è %.02f\n", prezzoUnitario);
         printf("Lo sconto applicato è del %d%%", sconto10);
     }
     else if(prezzoUnitario >= 50)
     {
         prezzoScontato = prezzoUnitario - (prezzoUnitario/100)*20;
-        printf("L'importo totale è %.02f\n", prezzoScontato);
-        printf("L'importo non scontato è %.02f\n", prezzoUnitario);
         printf("Lo sconto applicato è del %d%%", sconto20);
     }
+
+    printf("L'importo totale è %.02f\n", prezzoScontato);
+    printf("L'importo non scontato è %.02f\n", prezzoUnitario);
 
     return 0;
 }
